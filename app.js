d35b6c1dbc538c6671c5768e8df333a7bd6be082
rooksacrifice <82026397+rooksacrifice@users.noreply.github.com>
2024-08-14 22:34:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.getElementById('webhookForm').addEventListener('submit', function(event) {
+    event.preventDefault();
     var name = document.getElementById('name').value;
     var message = document.getElementById('message').value;
     var data = {
